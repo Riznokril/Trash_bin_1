909c518be0ccb1d98f57ba1e766087fd0671009d
Riznokril <48068349+Riznokril@users.noreply.github.com>
2021-09-24 04:56:03 +0300

    Revert "Added functions of structure: search element, delete element, add element, print all existing elements, delete all elements"
    This reverts commit a83131a3664cbb04a1b9882bbe52573244c57be6.

--- main.py
+++ main.py
@@ -1,51 +1,11 @@
 from structure import Binary_tree
 
-if __name__ == '__main__':
-    Tree = Binary_tree()
+Tree = Binary_tree()
 
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    Tree.add(1)
-
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    Tree.add(13)
-
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    Tree.add(0)
-
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    Tree.add(10)
-
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    Tree.add(3)
-
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    Tree.add(-5)
-
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    Tree.add(-2)
-
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    Tree.add(2)
-        
-
-    if Tree.search(2) == False:
-        print("There isn't this element")
-    else:
-        print("There is this element")
-    
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    print("--------------------------")
-    Tree.delete(1)
-    print("--------------------------")
-    print(Tree.print_binary_tree())
-    print("--------------------------")
-
+Tree.add(23)
+Tree.add(13)
+Tree.add(2)
+Tree.add(10)
+Tree.add(0)
+Tree.add(-5)
+print(Tree.search(10))
